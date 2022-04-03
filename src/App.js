bfef1505b3e15a75b8ca2d663a28359c6afb5e63
sazzad <hossensazzad1201@gmail.com>
2022-04-04 01:08:11 +0600

    router added in navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './component/Navbar/Navbar';
+import Home from './component/Home/Home';
+import Reviews from './component/Reviews/Reviews';
+import Dashboard from './component/Dashboard/Dashboard';
+import Blogs from './component/Blogs/Blogs';
+import About from './component/About/About';
+import NotFound from './component/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      {/* <Routes>
-        <Route path='/'></Route>
-      </Routes> */}
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
 
     </div>
   );
