96b485ca9a240af9d51ff75d7eb613dda75225df
sazzad <hossensazzad1201@gmail.com>
2022-04-04 05:46:38 +0600

    home page done

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from '../Review/Review';
 import './Home.css'
 
 const Home = () => {
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch(`data.json`)
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
     return (
-        <div className='car-container'>
-            <div className='title-container'>
-                <h1 className='title-one'>Your Dream Car</h1>
-                <h1 className='title-two'>Your Tesla Car</h1>
-                <p>Tesla, is an American electric vehicle and clean energy company based in Austin, Texas. Tesla designs and manufactures electric cars, battery energy storage from home to grid-scale, solar panels and solar roof tiles, and related products and services. Tesla is one of the world's most valuable companies and remains the world's most valuable automaker with a market capitalization of more than US $1 trillion.</p>
-            </div>
-            <div className='image-container'>
-                <img src="https://images.unsplash.com/photo-1617704548623-340376564e68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVzbGElMjBjYXJ8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+        <div >
+            <div className='car-container'>
+                <div className='title-container'>
+                    <h1 className='title-one'>Your Dream Car</h1>
+                    <h1 className='title-two'>Your Tesla Car</h1>
+                    <p>Tesla, is an American electric vehicle and clean energy company based in Austin, Texas. Tesla designs and manufactures electric cars, battery energy storage from home to grid-scale, solar panels and solar roof tiles, and related products and services. Tesla is one of the world's most valuable companies and remains the world's most valuable automaker with a market capitalization of more than US $1 trillion.</p>
+                </div>
+                <div className='image-container'>
+                    <img src="https://images.unsplash.com/photo-1617704548623-340376564e68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVzbGElMjBjYXJ8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+                </div>
 
             </div>
+            <div>
+                <h1 className='reviews'>Customer Reviews</h1>
+                <div className='reviews-container'>
+                    {
+                        reviews.map(review => <Review key={review.id} review={review}></Review>)
+                    }
+
+                </div>
+            </div>
+            <div className='reviews-btn-container'>
+                <button className='reviews-btn'>See All Reviews</button>
+            </div>
+
         </div>
     );
 };
---
+++ src/component/Review/Review.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Review.css'
+
+const Review = ({ review }) => {
+    // console.log(props.review);
+    const { name, comment, rating, img } = review
+    return (
+        <div >
+            <div className='review-detail'>
+                <img src={img} alt="" />
+                <h4 className='name'>Name: {name}</h4>
+                <p className='comment'>Comment: {comment}</p>
+                <p className='rating'>Rating: {rating}</p>
+
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default Review;
