53d63bc45f1797e7723b6e265f940b1e9bab8b8a
sazzad <hossensazzad1201@gmail.com>
2022-04-04 20:01:09 +0600

    dashboard done

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,11 +1,67 @@
+
 import React from 'react';
+import { Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css'
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        },
+
+    ];
     return (
-        <div>
-            <h2>Dashboard</h2>
+        <div className='chart'>
+            <LineChart width={400} height={400} data={data}>
+                <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                <Line dataKey="revenue"></Line>
+                <XAxis dataKey="sell" />
+                <Tooltip></Tooltip>
+                <YAxis />
+            </LineChart>
+            <PieChart width={400} height={400}>
+                <Pie data={data} dataKey="revenue" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                <Pie data={data} dataKey="sell" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                <Tooltip></Tooltip>
+            </PieChart>
+
         </div>
-    );
+
+    )
 };
 
 export default Dashboard;
